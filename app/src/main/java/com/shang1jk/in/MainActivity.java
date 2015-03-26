9132ccc26c88d0e39835dd8b36972194f8679808
shang1jk <shang1jk@gmail.com>
2015-03-26 17:01:01 +0800

    Initial commit

---
+++ app/src/main/java/com/shang1jk/in/MainActivity.java
@@ -0,0 +1,19 @@
+package com.shang1jk.in;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.net.DatagramPacket;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
