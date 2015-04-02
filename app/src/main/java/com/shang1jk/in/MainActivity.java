ae854709fe7160fa98a9af0e184b391b7517aa60
Shang1jk <shang1jk@gmail.com>
2015-04-02 12:31:48 +0800

    获取指定ip的mac地址。

--- app/src/main/java/com/shang1jk/in/MainActivity.java
+++ app/src/main/java/com/shang1jk/in/MainActivity.java
@@ -1,11 +1,16 @@
 package com.shang1jk.in;
 
-import android.support.v7.app.ActionBarActivity;
+import android.net.IpPrefix;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
 
-import java.net.DatagramPacket;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,6 +19,64 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("xxx", "xxx");
+
+        findViewById(R.id.scan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    scanLAN();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
+    /**
+     * scan LAN and deal the results
+     */
+    private void scanLAN() throws IOException {
+//        InetAddress
+//        InetSocketAddress
+
+//        import android.net.ConnectivityManager;
+//        import android.net.DhcpInfo;
+//        import android.net.NetworkInfo;
+//        import android.net.NetworkInfo.State;
+//        import android.net.wifi.SupplicantState;
+//        import android.net.wifi.WifiInfo;
+//        import android.net.wifi.WifiManager;
+//        import android.net.wifi.WifiManager.WifiLock;
+
+
+//        DhcpInfo dhcpInfo = new DhcpInfo();
+
+        Runtime runtime = Runtime.getRuntime();
+        String ip = "192.168.199.190";
+        Process exec = runtime.exec("ping " + ip + "-w 3 -n 3");
+
+
+        String mac = getMac(ip);
+        Log.e("MAC", mac);
+    }
+
+    private String getMac(String ip) throws SocketException, UnknownHostException {
+        NetworkInterface ni = NetworkInterface.getByInetAddress(InetAddress.getByName(ip));
+        byte[] mac = ni.getHardwareAddress();
+        StringBuffer sb = new StringBuffer();
+        for (byte b : mac) {
+            int i = b & 0xFF; //转为正整数
+            String s = Integer.toHexString(i).toUpperCase() ;
+            sb.append(s.length() == 1 ? 0 + s : s);
+            sb.append(":");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
 }
