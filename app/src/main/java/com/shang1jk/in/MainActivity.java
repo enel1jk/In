0dda8fed3287244c105b47c223eb433f0f2c7f66
Shang1jk <shang1jk@gmail.com>
2015-04-02 18:28:39 +0800

    获取一台的MAC.

--- app/src/main/java/com/shang1jk/in/MainActivity.java
+++ app/src/main/java/com/shang1jk/in/MainActivity.java
@@ -1,6 +1,5 @@
 package com.shang1jk.in;
 
-import android.net.IpPrefix;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -14,6 +13,7 @@ import java.net.UnknownHostException;
 
 
 public class MainActivity extends ActionBarActivity {
+    private final String ip = "192.168.1.107";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +25,7 @@ public class MainActivity extends ActionBarActivity {
         findViewById(R.id.scan).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    scanLAN();
-                } catch (UnknownHostException e) {
-                    e.printStackTrace();
-                } catch (SocketException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                scanLAN();
             }
         });
     }
@@ -41,38 +33,37 @@ public class MainActivity extends ActionBarActivity {
     /**
      * scan LAN and deal the results
      */
-    private void scanLAN() throws IOException {
-//        InetAddress
-//        InetSocketAddress
+    private void scanLAN() {
+        /*isReachable*/
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean reachable = false;
+                try {
+                    reachable = InetAddress.getByName(ip).isReachable(3000);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (reachable) {
+                    Log.e("isReachable", "yes");
+                } else {
+                    Log.e("isReachable", "no");
+                }
+            }
+        }).start();
 
-//        import android.net.ConnectivityManager;
-//        import android.net.DhcpInfo;
-//        import android.net.NetworkInfo;
-//        import android.net.NetworkInfo.State;
-//        import android.net.wifi.SupplicantState;
-//        import android.net.wifi.WifiInfo;
-//        import android.net.wifi.WifiManager;
-//        import android.net.wifi.WifiManager.WifiLock;
 
-
-//        DhcpInfo dhcpInfo = new DhcpInfo();
-
-        Runtime runtime = Runtime.getRuntime();
-        String ip = "192.168.199.190";
-        Process exec = runtime.exec("ping " + ip + "-w 3 -n 3");
-
-
-        String mac = getMac(ip);
-        Log.e("MAC", mac);
+//        String mac = getMac(ip);
+//        Log.e("MAC", mac);
     }
 
-    private String getMac(String ip) throws SocketException, UnknownHostException {
+    private String getMac(String ip) throws UnknownHostException, SocketException {
         NetworkInterface ni = NetworkInterface.getByInetAddress(InetAddress.getByName(ip));
         byte[] mac = ni.getHardwareAddress();
         StringBuffer sb = new StringBuffer();
         for (byte b : mac) {
             int i = b & 0xFF; //转为正整数
-            String s = Integer.toHexString(i).toUpperCase() ;
+            String s = Integer.toHexString(i).toUpperCase();
             sb.append(s.length() == 1 ? 0 + s : s);
             sb.append(":");
         }
