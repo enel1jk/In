3bec396fc981e3807e2d0ca6ffc490b38d2493e7
Shang1jk <shang1jk@gmail.com>
2015-04-03 17:47:24 +0800

    修改为ping

--- app/src/main/java/com/shang1jk/in/MainActivity.java
+++ app/src/main/java/com/shang1jk/in/MainActivity.java
@@ -6,22 +6,23 @@ import android.util.Log;
 import android.view.View;
 
 import java.io.IOException;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 
 public class MainActivity extends ActionBarActivity {
     private final String ip = "192.168.1.107";
+    private Runtime runtime = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("xxx", "xxx");
-
+        runtime = Runtime.getRuntime();
         findViewById(R.id.scan).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,40 +35,88 @@ public class MainActivity extends ActionBarActivity {
      * scan LAN and deal the results
      */
     private void scanLAN() {
-        /*isReachable*/
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                boolean reachable = false;
                 try {
-                    reachable = InetAddress.getByName(ip).isReachable(3000);
-                } catch (IOException e) {
+                    String localhost = getLocalhost();
+                    String ipPerfix = localhost.substring(0, localhost.lastIndexOf('.') + 1);
+                    Log.e(getLocalClassName(), "localhost:" + localhost);
+                    for (int i = 0; i < 256; i++) {
+                        String ip = ipPerfix + i;
+                        String mac = "";
+                        //TODO: 减小超时时间
+                        Process exec = runtime.exec("ping -c 1 -W 1 " + ip);    //ping 1 次，超时时间 1 秒
+                        boolean reachable = (exec.waitFor() == 0);
+
+                        InetAddress inetAddress = InetAddress.getByName(ip);
+                        if (reachable) {
+                            mac = getMac(inetAddress);
+                            if (mac != null) {
+                                Log.e(getLocalClassName(), "ip:" + ip + ", mac:" + mac);
+                            }
+                        } else {
+                            Log.e(getLocalClassName(), "ip:: " + ip + " isn't reachable");
+                        }
+                    }
+                } catch (Exception e) {
                     e.printStackTrace();
-                }
-                if (reachable) {
-                    Log.e("isReachable", "yes");
-                } else {
-                    Log.e("isReachable", "no");
                 }
             }
         }).start();
-
-
-//        String mac = getMac(ip);
-//        Log.e("MAC", mac);
     }
 
-    private String getMac(String ip) throws UnknownHostException, SocketException {
-        NetworkInterface ni = NetworkInterface.getByInetAddress(InetAddress.getByName(ip));
-        byte[] mac = ni.getHardwareAddress();
-        StringBuffer sb = new StringBuffer();
-        for (byte b : mac) {
-            int i = b & 0xFF; //转为正整数
-            String s = Integer.toHexString(i).toUpperCase();
-            sb.append(s.length() == 1 ? 0 + s : s);
-            sb.append(":");
+    /**
+     * 获取指定地址对应的mac
+     *
+     * @param inetAddress
+     * @return
+     */
+    private String getMac(InetAddress inetAddress) {
+        try {
+            NetworkInterface ni = NetworkInterface.getByInetAddress(inetAddress);
+            byte[] mac = ni.getHardwareAddress();
+            StringBuffer sb = new StringBuffer();
+            for (byte b : mac) {
+                int i = b & 0xFF; //转为正整数
+                String s = Integer.toHexString(i).toUpperCase();
+                sb.append(s.length() == 1 ? 0 + s : s);
+                sb.append(":");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            return sb.toString();
+        } catch (NullPointerException e) {
+            Log.e("error", inetAddress.getHostAddress());
+            e.printStackTrace();
+        } catch (SocketException e) {
+            e.printStackTrace();
         }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
+        return null;
+    }
+
+    /**
+     * @return 本地ip
+     */
+    private String getLocalhost() {
+        String localIp = null;
+        try {
+            Enumeration allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (allNetInterfaces.hasMoreElements()) {
+                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
+                if (netInterface.getName().toLowerCase().contains("wlan")) {
+                    Enumeration<InetAddress> inetAddresses = netInterface.getInetAddresses();
+                    while (inetAddresses.hasMoreElements()) {
+                        InetAddress inetAddress = inetAddresses.nextElement();
+                        if (inetAddress != null && inetAddress instanceof Inet4Address) {
+                            localIp = inetAddress.getHostAddress();
+                        }
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return localIp;
     }
 }
