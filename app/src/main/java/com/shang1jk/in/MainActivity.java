f7c726864ab5fae43796a12a236d342fcb6d3d43
Shang1jk <shang1jk@gmail.com>
2015-04-10 17:33:09 +0800

    改用ButterKnife

--- app/src/main/java/com/shang1jk/in/MainActivity.java
+++ app/src/main/java/com/shang1jk/in/MainActivity.java
@@ -4,13 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.View;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.ThreadPoolExecutor;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends ActionBarActivity {
     private final int IP_COUNT = 256 - 2 - 2;   //256 - x.x.x.0 - x.x.x.255 - local - gateway
@@ -22,68 +24,33 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        findViewById(R.id.scan).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        local = NetUtil.getLocal(MainActivity.this);
-                        gateway = NetUtil.getGateway(MainActivity.this);
-                        UdpProber udpProber = new UdpProber(MainActivity.this);
-                        udpProber.start();
-
-                        ThreadPoolExecutor executor = udpProber.getExecutor();
-                        while (true) {
-                            if (executor.getCompletedTaskCount() >= IP_COUNT) {
-                                executor.shutdown();
-                                readArp();
-                                break;
-                            }
-                        }
-                    }
-                }).start();
-            }
-        });
-
-        findViewById(R.id.read).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                readArp();
-            }
-        });
-
-
-        findViewById(R.id.scan_task).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent service = new Intent(MainActivity.this, ScanService.class);
-                startService(service);
-
-//                //TODO: 做成service？
-//                Timer timer = new Timer("scan_timer", true);
-//                TimerTask task = new TimerTask() {
-//                    @Override
-//                    public void run() {
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                findViewById(R.id.scan).performClick();
-//                            }
-//                        });
-//                    }
-//                };
-//                if (!isScanning) {
-//                    timer.schedule(task, 0, 2 * 60 * 1000);
-//                    isScanning = true;
-//                }
-            }
-        });
-
+        ButterKnife.inject(this);
     }
 
-    private void readArp() {
+    @OnClick(R.id.scan)
+    public void scanOnce() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                local = NetUtil.getLocal(MainActivity.this);
+                gateway = NetUtil.getGateway(MainActivity.this);
+                UdpProber udpProber = new UdpProber(MainActivity.this);
+                udpProber.start();
+
+                ThreadPoolExecutor executor = udpProber.getExecutor();
+                while (true) {
+                    if (executor.getCompletedTaskCount() >= IP_COUNT) {
+                        executor.shutdown();
+                        readArp();
+                        break;
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @OnClick(R.id.read)
+    public void readArp() {
         try {
             FileReader reader = new FileReader(ARP_PATH);
             BufferedReader bufferedReader = new BufferedReader(reader);
@@ -98,5 +65,15 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    @OnClick(R.id.scan_task_start)
+    public void startScanTask() {
+        Intent service = new Intent(MainActivity.this, ScanService.class);
+        startService(service);
+    }
 
+    @OnClick(R.id.scan_task_stop)
+    public void stopScanTask() {
+        Intent service = new Intent(MainActivity.this, ScanService.class);
+        stopService(service);
+    }
 }
