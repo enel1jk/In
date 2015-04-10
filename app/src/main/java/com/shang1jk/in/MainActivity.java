6e769c8c0821fb00e1d8af8a554a83c73925aa1e
Shang1jk <shang1jk@gmail.com>
2015-04-10 16:41:15 +0800

    可配置的定时扫描。（只能固定频率，不能在上下班时间点击附近提高频率）

--- app/src/main/java/com/shang1jk/in/MainActivity.java
+++ app/src/main/java/com/shang1jk/in/MainActivity.java
@@ -1,8 +1,6 @@
 package com.shang1jk.in;
 
-import android.net.DhcpInfo;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -12,20 +10,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class MainActivity extends ActionBarActivity {
     private final int IP_COUNT = 256 - 2 - 2;   //256 - x.x.x.0 - x.x.x.255 - local - gateway
     private final String ARP_PATH = "/proc/net/arp";
     private EndPoint local, gateway;
+    boolean isScanning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +29,9 @@ public class MainActivity extends ActionBarActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        local = getLocal();
-                        gateway = getGateway();
-                        UdpProber udpProber = new UdpProber();
+                        local = NetUtil.getLocal(MainActivity.this);
+                        gateway = NetUtil.getGateway(MainActivity.this);
+                        UdpProber udpProber = new UdpProber(MainActivity.this);
                         udpProber.start();
 
                         ThreadPoolExecutor executor = udpProber.getExecutor();
@@ -51,7 +42,6 @@ public class MainActivity extends ActionBarActivity {
                                 break;
                             }
                         }
-
                     }
                 }).start();
             }
@@ -64,6 +54,33 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
+
+        findViewById(R.id.scan_task).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent service = new Intent(MainActivity.this, ScanService.class);
+                startService(service);
+
+//                //TODO: 做成service？
+//                Timer timer = new Timer("scan_timer", true);
+//                TimerTask task = new TimerTask() {
+//                    @Override
+//                    public void run() {
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                findViewById(R.id.scan).performClick();
+//                            }
+//                        });
+//                    }
+//                };
+//                if (!isScanning) {
+//                    timer.schedule(task, 0, 2 * 60 * 1000);
+//                    isScanning = true;
+//                }
+            }
+        });
+
     }
 
     private void readArp() {
@@ -71,13 +88,9 @@ public class MainActivity extends ActionBarActivity {
             FileReader reader = new FileReader(ARP_PATH);
             BufferedReader bufferedReader = new BufferedReader(reader);
             String s = null;
-            int pointCount = 0;
             while ((s = bufferedReader.readLine()) != null) {
-                pointCount++;
                 Log.e("arp table", s);
             }
-//            Log.e("point count: ", "" + pointCount);
-            System.out.println("" + pointCount);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -85,124 +98,5 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    private static final short NETBIOS_UDP_PORT = 137;
-    // NBT UDP PACKET: QUERY; REQUEST; UNICAST
-    private static final byte[] NETBIOS_REQUEST =
-            {
-                    (byte) 0x82, (byte) 0x28, (byte) 0x0, (byte) 0x0, (byte) 0x0,
-                    (byte) 0x1, (byte) 0x0, (byte) 0x0, (byte) 0x0, (byte) 0x0,
-                    (byte) 0x0, (byte) 0x0, (byte) 0x20, (byte) 0x43, (byte) 0x4B,
-                    (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41,
-                    (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41,
-                    (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41,
-                    (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41,
-                    (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41,
-                    (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41, (byte) 0x41,
-                    (byte) 0x0, (byte) 0x0, (byte) 0x21, (byte) 0x0, (byte) 0x1
-            };
-
-
-    /**
-     * arp广播
-     */
-    private class UdpProber extends Thread {
-        private static final int PROBER_THREAD_POOL_SIZE = 25;
-
-        private class SingleProber extends Thread {
-            private InetAddress mAddress = null;
-
-            public SingleProber(InetAddress address) {
-                mAddress = address;
-            }
-
-            @Override
-            public void run() {
-                try {
-                    DatagramSocket socket = new DatagramSocket();
-                    DatagramPacket packet = new DatagramPacket(NETBIOS_REQUEST, NETBIOS_REQUEST.length, mAddress, NETBIOS_UDP_PORT);
-
-                    socket.setSoTimeout(1000);
-                    socket.send(packet);
-
-                    socket.close();
-                } catch (Exception ignored) {
-                }
-
-            }
-        }
-
-        private ThreadPoolExecutor mExecutor = null;
-
-        public UdpProber() {
-            mExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(PROBER_THREAD_POOL_SIZE);
-        }
-
-        @Override
-        public void run() {
-            Log.i(getClass().getSimpleName(), "UdpProber started ...");
-
-            byte[] localIpByteArray = local.getIpByteArray();
-
-            //x.x.x.1~x.x.x.254
-            for (int i = 1; i < 255; i++) {
-                if (i != local.getIpByteArray()[3] && i != gateway.getIpByteArray()[3]) {
-                    try {
-                        InetAddress byAddress = Inet4Address.getByAddress(localIpByteArray);
-                        mExecutor.execute(new SingleProber(byAddress));
-                    } catch (UnknownHostException e) {
-                        e.printStackTrace();
-                    }
-                    localIpByteArray[3]++;
-                }
-            }
-        }
-
-        public ThreadPoolExecutor getExecutor() {
-            return mExecutor;
-        }
-
-    }
-
-    /**
-     * @return 本地ip、mac
-     */
-    private EndPoint getLocal() {
-        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-        DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
-        int ipAddress = dhcpInfo.ipAddress;
-        //ip
-        String localIp = EndPoint.convertIp2String(ipAddress);
-        //mac
-        String localMac = null;
-        try {
-            NetworkInterface mInterface = NetworkInterface.getByInetAddress(Inet4Address.getByAddress(EndPoint.convertIp2ByteArray(ipAddress)));
-            byte[] hardwareAddress = mInterface.getHardwareAddress();
-            localMac = EndPoint.convertMac2String(hardwareAddress);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
-        Log.i(getLocalClassName(), "local: " + localIp + "\t" + localMac);
-        return new EndPoint(localIp, localMac);
-    }
-
-    /**
-     * @return 网关ip、mac
-     */
-    private EndPoint getGateway() {
-        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
-        DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
-        WifiInfo connectionInfo = wifiManager.getConnectionInfo();
-        int gateway = dhcpInfo.gateway;
-        //ip
-        String gatewayIp = EndPoint.convertIp2String(gateway);
-        //mac
-        String gatewayMac = connectionInfo.getBSSID();
-
-        Log.i(getLocalClassName(), "gateway: " + gatewayIp + "\t" + gatewayMac.toUpperCase());
-        return new EndPoint(gatewayIp, gatewayMac);
-    }
 
 }
